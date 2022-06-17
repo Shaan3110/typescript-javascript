4c18316e89ffb7d93f771cde8c945835282dffba
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-06-17 15:41:44 +0530

    varialbles

---
+++ syntaxes/variables/variables.js
@@ -0,0 +1,9 @@
+let a = "check";
+let b= 2;
+let c= {
+    h: "element"
+};
+let d= [1,2,3];
+let e= false;
+let f= ["check",2]
+let g= c.h;
---
+++ syntaxes/variables/variables.ts
@@ -0,0 +1,9 @@
+let a1:string = "check";
+let b1:number= 2;
+let c1:object= {
+    h: "element"
+};
+let d1:Array<number>= [1,2,3];
+let e1:boolean= false;
+let f1:[string,number]= ["check",2] //tuple
+let g1:string= c.h;
